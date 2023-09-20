6a352264820b7461ed3fc602c20f56ddb749906c
Rodrigo Perez <rodrigoperez0109@gmail.com>
2023-09-19 23:18:29 -0300

    claity scipt

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,13 @@ import '@/styles/globals.css'
 import Head from 'next/head'
 import { useEffect } from 'react';
 import AOS from "aos";
+import Script from 'next/script'
+
 
 import "aos/dist/aos.css";
 
 export default function App({ Component, pageProps }) {
-
+  
   useEffect(() => {
     AOS.init({
       easing: "ease-out-cubic",
@@ -15,22 +17,24 @@ export default function App({ Component, pageProps }) {
       offset: 50,
     });
   }, []);
-
+  
+  const clairtyCode = `
+  (function (c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "xxxxxxxxxxx");
+  `
+  
   return (
     <>
       <Head>
         <title>Evangelina Perez</title>
         <meta name="description" content="clases de canto zona sur Lomas de Zamora" />
         <link rel="icon" href="/tab.png" />
-        <script type="text/javascript">
-          {
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "ixvxdpl9kj")
-          }
-        </script>
+        <Script id="ms-clarity" strategy="beforeInteractive">
+            {clairtyCode}
+        </Script>
       </Head>
       <Layout>
         <Component {...pageProps} />
