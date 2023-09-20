f865b3529e90d30b1adfdd937eaa75250d2bb30d
Rodrigo Perez <rodrigoperez0109@gmail.com>
2023-09-19 23:07:10 -0300

    microsoft clarity script added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,6 +22,15 @@ export default function App({ Component, pageProps }) {
         <title>Evangelina Perez</title>
         <meta name="description" content="clases de canto zona sur Lomas de Zamora" />
         <link rel="icon" href="/tab.png" />
+        <script type="text/javascript">
+          {
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ixvxdpl9kj")
+          }
+        </script>
       </Head>
       <Layout>
         <Component {...pageProps} />
