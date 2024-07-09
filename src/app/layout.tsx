3f3e0f9d25bca2348e9581154306f37854f183e2
rodrigoperez01093 <rodrigoperez0109@gmail.com>
2024-07-08 23:56:32 -0300

    meta tags for seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,27 @@ export default function RootLayout({
       
       <head /> 
         <link rel="icon" href={`/tab.png`} />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Clases de canto personalizadas para todos los niveles. Aprende técnicas vocales y mejora tu voz con una profesora experimentada." />
+        <meta name="keywords" content="clases de canto, técnica vocal, lecciones de canto, profesora de canto, canto para principiantes, entrenamiento vocal" />
+        <meta name="author" content="Evangelina Pérez" />
+        <link rel="icon" href={`/tab.png`} />
+        <title>Evangelina Pérez</title>
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Clases de Canto con Evangelina Pérez" />
+        <meta property="og:description" content="Clases de canto personalizadas para todos los niveles. Aprende técnicas vocales y mejora tu voz con una profesora experimentada." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.evangelinaperez.com" />
+        <meta property="og:image" content="https://www.evangelinaperez.com/imagen.png" />
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Clases de Canto con Evangelina Pérez" />
+        <meta name="twitter:description" content="Clases de canto personalizadas para todos los niveles. Aprende técnicas vocales y mejora tu voz con una profesora experimentada." />
+        <meta name="twitter:image" content="https://www.evangelinaperez.com/imagen.png" />
       <head />
 
-      <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+      <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`} suppressHydrationWarning={true}>
         <Providers>
           {children}
           <Footer />
